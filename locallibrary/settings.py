7f01c9b8a836cac60d8b9b2beda39bc19827f9c6
Geepeee <rakeshgowdap16@gmail.com>
2020-10-31 19:29:37 +0530

    Added Environ Variables

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 #SECRET_KEY = 'lq7$05)i@*^0b@&^(mi33$3a!*of=2@di6o359q02+dsfxnx*2'
-SECRET_KEY = os.environ.get('Django_SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
